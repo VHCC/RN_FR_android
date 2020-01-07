872867331b5238473a8580baea1cc8b4c306974c
Ichen.tsu <r99521320@gmail.com>
2020-01-07 10:54:31 +0800

    [resolved] change android support to androidx

--- android/app/src/main/java/opencv/android/FaceCameraView.java
+++ android/app/src/main/java/opencv/android/FaceCameraView.java
@@ -6,7 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
